3bf2b5e0ebc98cfc1e28297b4ed943f734a7bf1f
nqhuy213 <nqhuy213@gmail.com>
2021-05-07 10:17:45 +1000

    Add popup

---
+++ views/index.js
@@ -0,0 +1,8 @@
+var e = document.getElementById('dot');
+
+e.onmouseover = function () {
+  document.getElementById('popup').style.display = 'block';
+};
+e.onmouseout = function () {
+  document.getElementById('popup').style.display = 'none';
+};
