ce4dfcf0ce79781412a99b948948edd47c3f149c
Nguyen Trong Dat <49813515+NGUYENTRONGDAT123@users.noreply.github.com>
2021-06-29 00:56:33 +1000

    Prepare Fasta files
    - Generate Assembled Contigs from 100 samples and store it as fasta-format files
    - create a python file to generate 100 samples (sample id 94 to 193)
    - create a bash script file to run amrfinderplus with multiple fasta-format files as input

---
+++ datasets/contig_fasta/AMRGenesGenerator.sh
@@ -0,0 +1,7 @@
+for assembly in *.fasta
+do
+    #get the base of the filename
+    base=$(basename $assembly .fasta)
+    # run AMRFinderPlus with output going to the 
+    amrfinder -n $assembly -O Staphylococcus_aureus -o amrfinder_result/amrfinder_$base.csv
+done
