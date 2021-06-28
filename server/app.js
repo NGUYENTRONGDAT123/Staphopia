d8557d5424691018db995cae7b0849efece808dc
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-06-28 16:37:22 +1000

    deploy

--- server/app.js
+++ server/app.js
@@ -18,13 +18,10 @@ app.use (logger ('dev'));
 app.use (express.json ());
 app.use (express.urlencoded ({extended: true}));
 app.use (cookieParser ());
-app.use (express.static (path.join (__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/build')))
 
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.use (cors ());
 
 // connect to the Database
