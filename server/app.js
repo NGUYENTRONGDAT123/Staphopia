68262b49ca02abc49e7addffae5a66909d55f63e
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-06-28 16:19:37 +1000

    Deploy

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ app.use (express.json ());
 app.use (express.urlencoded ({extended: true}));
 app.use (cookieParser ());
 app.use (express.static (path.join (__dirname, 'public')));
+app.use (express.static (path.join (__dirname, '../client/build')));
 app.use (cors ());
 
 // connect to the Database
