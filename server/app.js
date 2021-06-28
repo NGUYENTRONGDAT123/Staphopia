c231ad8c712dfc5079795225cc05f385ebbda2dd
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-06-28 16:26:57 +1000

    Try to build

--- server/app.js
+++ server/app.js
@@ -19,7 +19,12 @@ app.use (express.json ());
 app.use (express.urlencoded ({extended: true}));
 app.use (cookieParser ());
 app.use (express.static (path.join (__dirname, 'public')));
-app.use (express.static (path.join (__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use (cors ());
 
 // connect to the Database
