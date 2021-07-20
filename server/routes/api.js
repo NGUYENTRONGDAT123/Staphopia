e5622b1d0f9ea64ff2682e6cbf72b4d13105bf74
Viet Hoang Do <n10329935@qut.edu.au>
2021-07-20 17:43:41 +1000

    get list of samples and subclasses

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,7 +7,10 @@ const {promisify} = require ('util');
 const redis_url = process.env.REDIS_URL || 'redis://127.0.0.1';
 const redis_client = redis.createClient (redis_url);
 const redis_get = promisify (redis_client.get).bind (redis_client);
-
+redis_client.flushdb (function (err, succeeded) {
+  if (succeeded === 'OK') console.log ('Redis cache is clear');
+  else console.log ('Fail to clear Redis cache');
+});
 // router.get ('/regions', async (req, res, next) => {
 //   const covid19jhu = req.app.mongodb.db ('covid19jhu');
 //   let country = req.query.country === undefined ? '' : req.query.country;
@@ -93,34 +96,287 @@ router.get ('/amr-sample', async (req, res, next) => {
   const AMR = req.app.mongodb.db ('AMR');
   let samples = null;
   let key = null;
+
+  // Get all data if query not found
   try {
-    samples = req.query.samples === undefined
-      ? ['']
-      : JSON.parse (req.query.samples.toString ());
+    if (req.query.samples === undefined) {
+      pipeline = [
+        {
+          $match: {Name: {$exists: true}},
+        },
+      ];
+    } else {
+      samples = JSON.parse (req.query.samples.toString ());
+      key = samples.join ('-');
+      for (var i = 0; i < samples.length; i++) {
+        samples[i] = samples[i] + '.csv';
+      }
+      pipeline = [
+        {
+          $match: {Name: {$in: samples}},
+        },
+      ];
+    }
   } catch (err) {
-    res.status (400).json ({message: 'Bad request'});
-    throw err;
+    res.status (400).send ({error: true, message: 'Bad request!'});
   }
 
-  key = samples.join ('-');
-  for (var i = 0; i < samples.length; i++) {
-    samples[i] = samples[i] + '.csv';
-  }
+  // try to get data from redis
+  redis_get (key)
+    .then (async result => {
+      if (result) {
+        res.status (200).json (JSON.parse (result));
+        throw `Caught '${key}' in cache`;
+      } else {
+        // return data from mongodb
+        return AMR.collection ('100Genes').aggregate (pipeline).toArray ();
+      }
+    })
+    .then (async result => {
+      // store the result from mongodb to redis
+      redis_client.setex (
+        key,
+        28800,
+        JSON.stringify ({source: 'Redis Cache', result: result})
+      );
 
-  // Get specific samples if query found
+      // send the result back to client
+      return res.send ({source: 'Mongodb', result: result});
+    })
+    .catch (err => {
+      console.log (err);
+    });
+});
+
+router.get ('/available-sample', async (req, res, next) => {
+  const AMR = req.app.mongodb.db ('AMR');
+
+  let key = 'available-sample';
   let pipeline = [
     {
-      $match: {Name: {$in: samples}},
+      $group: {
+        _id: '$Name',
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        sample_ids: {
+          $push: '$_id',
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
     },
   ];
 
+  // try to get data from redis
+  redis_get (key)
+    .then (async result => {
+      if (result) {
+        res.status (200).json (JSON.parse (result));
+        throw `Caught '${key}' in cache`;
+      } else {
+        // return data from mongodb
+        return AMR.collection ('100Genes').aggregate (pipeline).toArray ();
+      }
+    })
+    .then (async result => {
+      // store the result from mongodb to redis
+      result = result[0].sample_ids;
+      for (var i = 0; i < result.length; i++) {
+        result[i] = result[i].replace ('.csv', '');
+      }
+
+      // convert sample list to  integer and sort in ascending order
+      result = result.map (element => Number (element));
+      result.sort (function (a, b) {
+        return a - b;
+      });
+      redis_client.setex (
+        key,
+        28800,
+        JSON.stringify ({source: 'Redis Cache', result: result})
+      );
+
+      // send the result back to client
+      return res.send ({source: 'Mongodb', result: result});
+    })
+    .catch (err => {
+      console.log (err);
+    });
+});
+
+router.get ('/subclass-sample', async (req, res, next) => {
+  const AMR = req.app.mongodb.db ('AMR');
+  let samples = null;
+  let key = null;
+
   // Get all data if query not found
-  if (req.query.samples === undefined) {
-    pipeline = [
-      {
-        $match: {Name: {$exists: true}},
+  try {
+    if (req.query.samples === undefined) {
+      key = 'subclass-sample';
+      pipeline = [
+        {
+          $group: {
+            _id: '$Name',
+            subclasses: {
+              $push: '$Subclass',
+            },
+          },
+        },
+      ];
+    } else {
+      samples = JSON.parse (req.query.samples.toString ());
+      key = 'subclass-sample-' + samples.join ('-');
+      for (var i = 0; i < samples.length; i++) {
+        samples[i] = samples[i] + '.csv';
+      }
+      pipeline = [
+        {
+          $group: {
+            _id: '$Name',
+            subclasses: {
+              $push: '$Subclass',
+            },
+          },
+        },
+        {
+          $match: {_id: {$in: samples}},
+        },
+      ];
+    }
+  } catch (err) {
+    res.status (400).send ({error: true, message: 'Bad request!'});
+  }
+
+  // try to get data from redis
+  redis_get (key)
+    .then (async result => {
+      if (result) {
+        res.status (200).json (JSON.parse (result));
+        throw `Caught '${key}' in cache`;
+      } else {
+        // return data from mongodb
+        return AMR.collection ('100Genes').aggregate (pipeline).toArray ();
+      }
+    })
+    .then (async result => {
+      // store the result from mongodb to redis
+      redis_client.setex (
+        key,
+        28800,
+        JSON.stringify ({source: 'Redis Cache', result: result})
+      );
+
+      // send the result back to client
+      return res.send ({source: 'Mongodb', result: result});
+    })
+    .catch (err => {
+      console.log (err);
+    });
+});
+
+// localhost:8393/api/available-subclass
+router.get ('/available-subclass', async (req, res, next) => {
+  const AMR = req.app.mongodb.db ('AMR');
+
+  let key = 'available-subclass';
+  let pipeline = [
+    {
+      $group: {
+        _id: '$Subclass',
       },
-    ];
+    },
+    {
+      $group: {
+        _id: null,
+        subclasses: {
+          $addToSet: '$_id',
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+  ];
+
+  // try to get data from redis
+  redis_get (key)
+    .then (async result => {
+      if (result) {
+        res.status (200).json (JSON.parse (result));
+        throw `Caught '${key}' in cache`;
+      } else {
+        // return data from mongodb
+        return AMR.collection ('100Genes').aggregate (pipeline).toArray ();
+      }
+    })
+    .then (async result => {
+      // store the result from mongodb to redis
+      result = result[0].subclasses;
+      result = result.filter ((v, i, a) => a.indexOf (v) === i);
+      result.sort ();
+      redis_client.setex (
+        key,
+        28800,
+        JSON.stringify ({source: 'Redis Cache', result: result})
+      );
+
+      // send the result back to client
+      return res.send ({source: 'Mongodb', result: result});
+    })
+    .catch (err => {
+      console.log (err);
+    });
+});
+
+// e.g. localhost:8393/api/sample-subclass?subclasses=FOSFOMYCIN,QUATERNARY AMMONIUM
+// e.g. localhost:8393/api/sample-subclass
+router.get ('/sample-subclass', async (req, res, next) => {
+  const AMR = req.app.mongodb.db ('AMR');
+  let subclasses = null;
+  let key = null;
+
+  // Get all data if query not found
+  try {
+    if (req.query.subclasses === undefined) {
+      key = 'sample-subclass';
+      pipeline = [
+        {
+          $group: {
+            _id: '$Subclass',
+            samples: {
+              $addToSet: '$Name',
+            },
+          },
+        },
+      ];
+    } else {
+      subclasses = req.query.subclasses;
+      subclasses = subclasses.split (',');
+      key = 'sample-subclass-'.concat (subclasses.sort ().join ('-'));
+      pipeline = [
+        {
+          $group: {
+            _id: '$Subclass',
+            samples: {
+              $addToSet: '$Name',
+            },
+          },
+        },
+        {
+          $match: {_id: {$in: subclasses}},
+        },
+      ];
+    }
+  } catch (err) {
+    res.status (400).send ({error: true, message: 'Bad request!'});
   }
 
   // try to get data from redis
