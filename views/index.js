38bab341db254451e78e9d531290c5ad3e705c8c
hoangqwe159 <doviethoang159@gmail.com>
2021-08-05 17:02:04 +1000

    Try to deploy

--- views/index.js
+++
@@ -1,8 +0,0 @@
-var e = document.getElementById('dot');
-
-e.onmouseover = function () {
-  document.getElementById('popup').style.display = 'block';
-};
-e.onmouseout = function () {
-  document.getElementById('popup').style.display = 'none';
-};
