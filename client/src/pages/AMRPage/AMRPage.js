97f687564b434a61c54deac480325f846d0f6064
hoangqwe159 <doviethoang159@gmail.com>
2021-08-08 23:40:06 +1000

    search panel

--- client/src/pages/AMRPage/AMRPage.js
+++ client/src/pages/AMRPage/AMRPage.js
@@ -1,14 +1,90 @@
-import React from "react";
-import { Row, Col, Card } from "antd";
-import BubbleChart from "../../components/BubbleChart";
-import data from "../../TestingData/data2";
+import React, {useState} from 'react';
+import {Row, Col, Card, Menu, Input, Space} from 'antd';
+import BubbleChart from '../../components/BubbleChart';
+import data from '../../TestingData/data2';
+import 'antd/dist/antd.css';
 
-import "antd/dist/antd.css";
-export default function AMRPage() {
+const {Search} = Input;
+const SAMPLES = ['99', '100', '101', '102', '103', '104'];
+
+export default function AMRPage () {
+  const [availableSample, setAvailableSample] = useState (SAMPLES);
+  // const [toAddSample, setToAddSample] = useState ([]);
+
+  const [name, setName] = useState ('');
+  const [toAddSample, setToAddSample] = useState ([]);
+
+  const filter = e => {
+    const keyword = e.target.value;
+    const tempList = toAddSample.map((sample) => sample);
+    if (keyword !== '') {
+      const results = toAddSample.filter (sample => {
+        return sample.toLowerCase ().startsWith (keyword.toLowerCase ());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setToAddSample (results);
+    } else {
+      setToAddSample (tempList);
+      // If the text field is empty, show all samples
+    }
+
+    setName (keyword);
+  };
+
+
+  const handleDelete = value => {
+    if (value.key !== 'notFound') {
+      let sampleToDelete = value.key;
+      const newAvailableList = availableSample.filter (
+        sample => sample !== sampleToDelete
+      );
+      const newToAddList = toAddSample.concat (sampleToDelete);
+      setAvailableSample (newAvailableList);
+      setToAddSample (newToAddList);
+    }
+  };
+
+  const handleAdd = value => {
+    if (value.key !== 'notFound') {
+      let sampleToDelete = value.key;
+      const newToAddList = toAddSample.filter (
+        sample => sample !== sampleToDelete
+      );
+      const newAvailableList = availableSample.concat (sampleToDelete);
+      setAvailableSample (newAvailableList);
+      setToAddSample (newToAddList);
+    }
+  };
+
   return (
     <Row gutter={[8, 8]} type="flex">
       <Col span={5}>
-        <Card title={`Sample Selection`} />
+        <Card title={`Sample to Remove`}>
+          <Menu mode="inline" onClick={handleDelete}>
+            {availableSample && availableSample.length > 0
+              ? availableSample.map (sample => (
+                  <Menu.Item key={sample}>{sample}</Menu.Item>
+                ))
+              : <Menu.Item disabled={true} key="notFound">No results found</Menu.Item>}
+
+          </Menu>
+        </Card>
+        <Card title={`Sample to Add`}>
+          <input
+            type="search"
+            value={name}
+            onChange={filter}
+            className="input"
+            placeholder="Filter"
+          />
+          <Menu mode="inline" onClick={handleAdd}>
+            {toAddSample && toAddSample.length > 0
+              ? toAddSample.map (sample => (
+                  <Menu.Item key={sample}>{sample}</Menu.Item>
+                ))
+              : <Menu.Item disabled={true} key="notFound">No results found</Menu.Item>}
+          </Menu>
+        </Card>
       </Col>
       <Col span={13}>
         <Row gutter={[8, 8]}>
