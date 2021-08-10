43c235acc557624a5523dac80f5791e0cac4c2ff
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-08-10 19:04:29 +1000

    deploy

--- server/app.js
+++ server/app.js
@@ -32,8 +32,8 @@ app.use (cors ());
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
   });
 }
 
