b3a17ae08360d9b81672584b881e65b8473cc547
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-08-10 18:34:06 +1000

    configure deploy

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use (express.static (path.join (__dirname, '../client/build')));
 
 app.use (cors ());
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 
