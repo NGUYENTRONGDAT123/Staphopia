07736c70de5b11da982d18533d37e49955f35206
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2021-08-17 18:58:06 +1000

    handle redux

--- client/src/components/search-panel/SearchPanel.js
+++ client/src/components/search-panel/SearchPanel.js
@@ -1,97 +1,102 @@
-import React, { useState, useEffect } from "react";
-import { Card, Input, Button, Tree } from "antd";
-const { Search } = Input;
+import React, {useState, useEffect} from 'react';
+import {Card, Input, Button, Tree} from 'antd';
+const {Search} = Input;
 
-export default function SearchPanel(props) {
-  const { packedData, selectSample, deleteSample, restoreSample } = props;
+export default function SearchPanel (props) {
+  const {packedData, selectSample, deleteSample, restoreSample} = props;
 
-  const [data, setData] = useState([]);
-  const [dataList, setDataList] = useState([]);
-  const [restoreData, setRestoreData] = useState([]);
+  const [data, setData] = useState ([]);
+  const [dataList, setDataList] = useState ([]);
+  const [restoreData, setRestoreData] = useState ([]);
 
-  useEffect(() => {
-    var dataTemp = [];
-    var dataListTemp = [];
-    var restoreDataTemp = [];
-    if (packedData !== null) {
-      for (let i = 0; i < packedData.length; i++) {
-        if (packedData[i].name !== null) {
-          dataTemp.push({
-            title: packedData[i].name,
-            key: packedData[i].name,
-            children: [],
-          });
-
-          restoreDataTemp.push({
-            title: packedData[i].name,
-            key: packedData[i].name,
-            children: [],
-          });
-
-          dataListTemp.push({
-            title: packedData[i].name,
-            key: packedData[i].name,
-          });
-          for (let j = 0; j < packedData[i].children.length; j++) {
-            dataTemp[dataTemp.length - 1].children.push({
-              title: packedData[i].children[j].name,
-              key: packedData[i].name.concat(packedData[i].children[j].name),
+  useEffect (
+    () => {
+      var dataTemp = [];
+      var dataListTemp = [];
+      var restoreDataTemp = [];
+      if (packedData !== null) {
+        for (let i = 0; i < packedData.length; i++) {
+          if (packedData[i].name !== null) {
+            dataTemp.push ({
+              title: packedData[i].name,
+              key: packedData[i].name,
+              children: [],
             });
 
-            restoreDataTemp[restoreDataTemp.length - 1].children.push({
-              title: packedData[i].children[j].name,
-              key: packedData[i].name.concat(packedData[i].children[j].name),
+            restoreDataTemp.push ({
+              title: packedData[i].name,
+              key: packedData[i].name,
+              children: [],
             });
 
-            dataListTemp.push({
-              title: packedData[i].children[j].name,
-              key: packedData[i].name.concat(packedData[i].children[j].name),
+            dataListTemp.push ({
+              title: packedData[i].name,
+              key: packedData[i].name,
             });
+            for (let j = 0; j < packedData[i].children.length; j++) {
+              dataTemp[dataTemp.length - 1].children.push ({
+                title: packedData[i].children[j].name,
+                key: packedData[i].name.concat (packedData[i].children[j].name),
+                value: packedData[i].children[j].value,
+              });
+
+              restoreDataTemp[restoreDataTemp.length - 1].children.push ({
+                title: packedData[i].children[j].name,
+                key: packedData[i].name.concat (packedData[i].children[j].name),
+                value: packedData[i].children[j].value,
+              });
+
+              dataListTemp.push ({
+                title: packedData[i].children[j].name,
+                key: packedData[i].name.concat (packedData[i].children[j].name),
+              });
+            }
           }
         }
       }
-    }
-    setDataList(dataListTemp);
-    setData(dataTemp);
-    setRestoreData(restoreDataTemp);
-  }, [packedData]);
+      setDataList (dataListTemp);
+      setData (dataTemp);
+      setRestoreData (restoreDataTemp);
+    },
+    [packedData]
+  );
 
-  const [expandedKeys, setExpandedKeys] = useState([]);
-  const [checkedKeys, setCheckedKeys] = useState([]);
-  const [selectedKeys, setSelectedKeys] = useState([]);
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
-  const [searchValue, setSearchValue] = useState([]);
+  const [expandedKeys, setExpandedKeys] = useState ([]);
+  const [checkedKeys, setCheckedKeys] = useState ([]);
+  const [selectedKeys, setSelectedKeys] = useState ([]);
+  const [autoExpandParent, setAutoExpandParent] = useState (true);
+  const [searchValue, setSearchValue] = useState ([]);
 
-  const onChange = (e) => {
-    const { value } = e.target;
-    if (value !== "") {
+  const onChange = e => {
+    const {value} = e.target;
+    if (value !== '') {
       const expandedKeys = dataList
-        .map((item) => {
-          if (item.title.indexOf(value) > -1) {
-            return getParentKey(item.key, data);
+        .map (item => {
+          if (item.title.indexOf (value) > -1) {
+            return getParentKey (item.key, data);
           }
           return null;
         })
-        .filter((item, i, self) => item && self.indexOf(item) === i);
+        .filter ((item, i, self) => item && self.indexOf (item) === i);
 
       const checkedKeys = dataList
-        .map((item) => {
-          if (item.key.indexOf(value) > -1) {
+        .map (item => {
+          if (item.key.indexOf (value) > -1) {
             return item.key;
           }
           return null;
         })
-        .filter((item, i, self) => item && self.indexOf(item) === i);
+        .filter ((item, i, self) => item && self.indexOf (item) === i);
 
-      setExpandedKeys(expandedKeys);
-      setCheckedKeys(checkedKeys);
-      setSearchValue(value);
-      setAutoExpandParent(true);
+      setExpandedKeys (expandedKeys);
+      setCheckedKeys (checkedKeys);
+      setSearchValue (value);
+      setAutoExpandParent (true);
     } else {
-      setExpandedKeys([]);
-      setCheckedKeys([]);
-      setSearchValue(value);
-      setAutoExpandParent(true);
+      setExpandedKeys ([]);
+      setCheckedKeys ([]);
+      setSearchValue (value);
+      setAutoExpandParent (true);
     }
   };
 
@@ -100,90 +105,94 @@ export default function SearchPanel(props) {
     for (let i = 0; i < tree.length; i++) {
       const node = tree[i];
       if (node.children) {
-        if (node.children.some((item) => item.key === key)) {
+        if (node.children.some (item => item.key === key)) {
           parentKey = node.key;
-        } else if (getParentKey(key, node.children)) {
-          parentKey = getParentKey(key, node.children);
+        } else if (getParentKey (key, node.children)) {
+          parentKey = getParentKey (key, node.children);
         }
       }
     }
     return parentKey;
   };
 
-  const onExpand = (expandedKeysValue) => {
-    console.log("onExpand", expandedKeysValue); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+  const onExpand = expandedKeysValue => {
+    console.log ('onExpand', expandedKeysValue); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
 
-    setExpandedKeys(expandedKeysValue);
-    setAutoExpandParent(false);
+    setExpandedKeys (expandedKeysValue);
+    setAutoExpandParent (false);
   };
 
-  const onCheck = (checkedKeysValue) => {
-    console.log("onCheck", checkedKeysValue);
-    setCheckedKeys(checkedKeysValue);
+  const onCheck = checkedKeysValue => {
+    console.log ('onCheck', checkedKeysValue);
+    setCheckedKeys (checkedKeysValue);
   };
 
   const onSelect = (selectedKeysValue, info) => {
-    console.log("onSelect", selectedKeysValue);
-    setSelectedKeys(selectedKeysValue);
-    var matches = selectedKeysValue[0].match(/(\d+)/);
+    console.log ('onSelect', selectedKeysValue);
+    setSelectedKeys (selectedKeysValue);
+    var matches = selectedKeysValue[0].match (/(\d+)/);
 
     if (matches) {
-      selectSample(matches[0]);
+      selectSample (matches[0]);
     }
   };
 
-  const handleDeleteSelected = (value) => {
+  const handleDeleteSelected = value => {
     let sampleToDelete = checkedKeys;
-    console.log(restoreData);
-    let dataTemp1 = [...data];
-
-    let removedDataList = dataList.filter(function (item) {
-      return sampleToDelete.indexOf(item.key) <= -1;
-    });
-
-    console.log(removedDataList);
-
+    let dataTemp = [...data];
     for (let i = 0; i < data.length; i++) {
-      // for (let j = 0; j < data[i].children.length; j++) {
-      //   if (sampleToDelete.includes(data[i].children[j].key)) {
-
-      //   }
-      // }
       if (data[i] !== undefined) {
-        var filtered = data[i].children.filter(function (item) {
-          return sampleToDelete.indexOf(item.key) <= -1;
+        var filtered = data[i].children.filter (function (item) {
+          return sampleToDelete.indexOf (item.key) <= -1;
         });
-        dataTemp1[i].children = filtered;
+        dataTemp[i].children = filtered;
       }
     }
-    deleteSample(dataTemp1);
-    setData(dataTemp1);
+
+    let newPackedData = formatData(dataTemp);
+    deleteSample(newPackedData);
+    setData (dataTemp);
   };
 
-  const handleRestore = (value) => {
+  const formatData = dataTemp => {
+    let newPackedData = [];
+    for (let i = 0; i < dataTemp.length; i++) {
+      newPackedData.push ({
+        name: dataTemp[i].title,
+        children: [],
+      });
+      for (let j = 0; j < dataTemp[i].children.length; j++) {
+        newPackedData[newPackedData.length - 1].children.push ({
+          name: dataTemp[i].children[j].title,
+          value: dataTemp[i].children[j].value,
+        });
+      }
+    }
+    return newPackedData;
+  };
+
+  const handleRestore = value => {
     // let dataTemp2 = [...restoreData]
-    let dataTemp2 = JSON.parse(JSON.stringify(restoreData));
-    restoreSample(dataTemp2);
-    setData(dataTemp2);
+    let dataTemp = JSON.parse (JSON.stringify (restoreData));
+    let newPackedData = formatData(dataTemp);
+    restoreSample (newPackedData);
+    setData (dataTemp);
   };
 
-  const loop = (data) =>
-    data.map((item) => {
-      const index = item.title.indexOf(searchValue);
-      const beforeStr = item.title.substr(0, index);
-      const afterStr = item.title.substr(index + searchValue.length);
-      const title =
-        index > -1 ? (
-          <span>
+  const loop = data =>
+    data.map (item => {
+      const index = item.title.indexOf (searchValue);
+      const beforeStr = item.title.substr (0, index);
+      const afterStr = item.title.substr (index + searchValue.length);
+      const title = index > -1
+        ? <span>
             {beforeStr}
             <span className="site-tree-search-value">{searchValue}</span>
             {afterStr}
           </span>
-        ) : (
-          <span>{item.title}</span>
-        );
+        : <span>{item.title}</span>;
       if (item.children) {
-        return { title, key: item.key, children: loop(item.children) };
+        return {title, key: item.key, children: loop (item.children)};
       }
 
       return {
@@ -195,13 +204,13 @@ export default function SearchPanel(props) {
     <div>
       <Card title="Search Sample">
         <Search
-          style={{ marginBottom: 8 }}
+          style={{marginBottom: 8}}
           placeholder="Search"
           onChange={onChange}
         />
         <Tree
           checkable
-          style={{ overflow: "auto", height: "45vh" }}
+          style={{overflow: 'auto', height: '45vh'}}
           onExpand={onExpand}
           expandedKeys={expandedKeys}
           autoExpandParent={autoExpandParent}
@@ -209,21 +218,21 @@ export default function SearchPanel(props) {
           checkedKeys={checkedKeys}
           onSelect={onSelect}
           selectedKeys={selectedKeys}
-          treeData={loop(data)}
+          treeData={loop (data)}
         />
 
         <Button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleRestore();
+          onClick={e => {
+            e.stopPropagation ();
+            handleRestore ();
           }}
         >
           Restore
         </Button>
         <Button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleDeleteSelected();
+          onClick={e => {
+            e.stopPropagation ();
+            handleDeleteSelected ();
           }}
         >
           Delete Selected
--- client/src/pages/AMRPage/AMRPage.js
+++ client/src/pages/AMRPage/AMRPage.js
@@ -1,57 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Card } from "react-bootstrap";
-import BubbleChart from "../../components/bubble-chart";
-import SearchPanel from "../../components/search-panel";
-import SampleInfoPanel from "../../components/sample-info-panel";
+import React, {useState, useEffect} from 'react';
+import { Row, Col, Card, Menu, Input, List, Button } from "antd";
+import BubbleChart from '../../components/bubble-chart';
+import SearchPanel from '../../components/search-panel';
+import SampleInfoPanel from '../../components/sample-info-panel';
 // import data from "../../TestingData/data2";
-import "./AMRPage.css";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchPackedCircleData, fetchSelectedSample } from "../../api/AMRapi";
+import './AMRPage.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchPackedCircleData, fetchSelectedSample} from '../../api/AMRapi';
 import {
   dispatchDeleteSample,
   dispatchPackedCircleData,
   dispatchRestoreSample,
   selectSample,
-} from "../../redux/actions/visualization";
-import AmrTable from "../../components/AmrTable";
+} from '../../redux/actions/visualization';
+import AmrTable from '../../components/amr-table';
 
-export default function AMRPage() {
-  const [isLoadingPacked, setIsLoadingPacked] = useState(false);
-  const SampleInfoData = useSelector((state) => state.Visualization.sampleInfo);
-  const AMRTableData = useSelector((state) => state.Visualization.amrTable);
-  const SampleSelectData = useSelector(
-    (state) => state.Visualization.sampleSelection
+export default function AMRPage () {
+  const [isLoadingPacked, setIsLoadingPacked] = useState (false);
+  const SampleInfoData = useSelector (state => state.Visualization.sampleInfo);
+  const AMRTableData = useSelector (state => state.Visualization.amrTable);
+  const SampleSelectData = useSelector (
+    state => state.Visualization.sampleSelection
   );
-  const PackedCircleData = useSelector(
-    (state) => state.Visualization.packedCircleData
+  const PackedCircleData = useSelector (
+    state => state.Visualization.packedCircleData
   );
-  const dispatch = useDispatch();
+  const dispatch = useDispatch ();
 
-  useEffect(() => {
-    async function getPackedData() {
-      setIsLoadingPacked(true);
-      const result = await fetchPackedCircleData();
-      dispatch(dispatchPackedCircleData(result));
-      setIsLoadingPacked(false);
+  useEffect (() => {
+    async function getPackedData () {
+      setIsLoadingPacked (true);
+      const result = await fetchPackedCircleData ();
+      dispatch (dispatchPackedCircleData (result));
+      setIsLoadingPacked (false);
     }
-    getPackedData();
+    getPackedData ();
   }, []);
 
-  const handleSelectSample = async (sample) => {
-    const data = await fetchSelectedSample(sample);
-    dispatch(selectSample(data));
+  const handleSelectSample = async sample => {
+    const data = await fetchSelectedSample (sample);
+    dispatch (selectSample (data));
   };
 
-  const handleDeleteSample = (samples) => {
-    dispatch(dispatchDeleteSample(samples));
+  const handleDeleteSample = samples => {
+    dispatch (dispatchDeleteSample (samples));
   };
 
-  const handleRestoreSample = (samples) => {
-    dispatch(dispatchRestoreSample(samples));
+  const handleRestoreSample = samples => {
+    dispatch (dispatchRestoreSample (samples));
   };
   return (
-    <Row className="px-3">
-      <Col lg={3}>
+    <Row gutter={[8, 8]} type="flex">
+      <Col span={5}>
         <SearchPanel
           packedData={PackedCircleData}
           selectSample={handleSelectSample}
@@ -59,32 +59,38 @@ export default function AMRPage() {
           restorSample={handleRestoreSample}
         />
       </Col>
-      <Col lg={9}>
-        <Row>
-          <Col key="Bubble-chart" lg={9}>
-            <BubbleChart
-              width="900"
-              height="900"
-              data={PackedCircleData}
-              isLoading={isLoadingPacked}
-            />
-          </Col>
-          <Col lg={3}>
-            <Row>
-              <Col key="Sample-Info">
-                <SampleInfoPanel />
-              </Col>
-            </Row>
-            <Row>
-              <Col key="AMR-Statistic">
-                <div>alo</div>
-              </Col>
-            </Row>
+      <Col span={13}>
+        <Row gutter={[8, 8]}>
+          <Col key="Bubble-chart" span={24}>
+            <Card title="AMR Visualisations">
+              <BubbleChart
+                width="900"
+                height="900"
+                data={PackedCircleData}
+                isLoading={isLoadingPacked}
+              />
+            </Card>
           </Col>
         </Row>
-        <Row className="my-4">
-          <Col key="AMR-Table">
-            <AmrTable />
+        <Row gutter={[8, 8]}>
+          <Col key="AMR-Table" span={24}>
+            <Card title="AMR Table">
+              <AmrTable />
+            </Card>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={6}>
+        <Row gutter={[8, 8]}>
+          <Col key="Sample-Info" span={24}>
+            <Card title="Sample Information">
+              <SampleInfoPanel />
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={[8, 8]}>
+          <Col key="AMR-Statistic" span={24}>
+            <Card title="AMR Statistic" />
           </Col>
         </Row>
       </Col>
