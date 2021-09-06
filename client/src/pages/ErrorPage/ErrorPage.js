bc9549532e29572c6428efdf185e8c2fdcecb616
nqhuy213 <nqhuy213@gmail.com>
2021-09-06 15:29:02 +1000

    Fix font

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -4,11 +4,11 @@ import "./ErrorPage.css";
 export default function ErrorPage() {
   return (
     <div id="notfound">
-      <div class="notfound">
-        <div class="notfound-404">
+      <div className="notfound">
+        <div className="notfound-404">
           <h1>404</h1>
         </div>
-        <h2 className="mt-5">Oops! This Page Could Not Be Found</h2>
+        <h2 className="oops">Oops! This Page Could Not Be Found</h2>
         <p>
           Sorry but the page you are looking for does not exist, have been
           removed. name changed or is temporarily unavailable
